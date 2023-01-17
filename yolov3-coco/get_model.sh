87e8765d167d16b1fff86ee90949dd9914f5e75e
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2023-01-17 13:33:49 +0530

    Update get_model.sh

--- yolov3-coco/get_model.sh
+++ yolov3-coco/get_model.sh
@@ -7,7 +7,6 @@
 # └── datasets
 #     └── coco  ← downloads here
 
-echo 'Getting the YOLOv3 model'
 echo 'Starting Download...'
 wget --no-check-certificate https://pjreddie.com/media/files/yolov3.weights
 echo 'Download completed successfully!!'
