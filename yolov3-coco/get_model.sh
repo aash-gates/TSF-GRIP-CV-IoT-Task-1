e970261dc2509f62bf2493cfaf3fc15f976b4407
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2023-01-17 13:35:13 +0530

    Update get_model.sh

--- yolov3-coco/get_model.sh
+++ yolov3-coco/get_model.sh
@@ -19,3 +19,4 @@ d='../datasets/coco/images' # unzip directory
 url=http://images.cocodataset.org/zips/
 f1='train2017.zip' # 19G, 118k images
 f2='val2017.zip'   # 1G, 5k images
+f3='test2017.zip'  # 7G, 41k images (optional)
