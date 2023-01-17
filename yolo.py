7a5c06a2e942217659962203e73b1e17cf8ec0df
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2023-01-17 13:32:17 +0530

    Update yolo.py

--- yolo.py
+++ yolo.py
@@ -84,7 +84,6 @@ if __name__ == '__main__':
 
 	net = cv.dnn.readNetFromDarknet(FLAGS.config, FLAGS.weights)
 
-	layer_names = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
         
 	if FLAGS.image_path is None and FLAGS.video_path is None:
 	    print ('Neither path to an image or path to video provided')
